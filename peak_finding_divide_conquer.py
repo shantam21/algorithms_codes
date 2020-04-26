4155b24f17e05885efd963e6995706b04cc3c0f7
shansaxena <shansaxena@deloitte.com>
2020-04-26 21:45:23 +0530

    completed the peak finding implementation with divide and conquer

--- peak_finding_divide_conquer.py
+++ peak_finding_divide_conquer.py
@@ -1,12 +1,35 @@
 # for monotonically inceeasing then decreasing sequence of arrays
 
-arr = [2,4,6,7,8,5,4,1]
+arr = [-1, -3, 7, 20, 4, 1, 0]
 
 def find_peak(arr):
-"""[summary]
+    """Finds peak for a monotonicaly increasing then decreasing function
 
-Arguments:
-    arr {[type]} -- [description]
-"""
-    
+    Arguments:
+        arr {[array]} -- Array of sequence
+
+    Returns:
+        [int]/[float] -- peak for the sequence
+    """    
+    # print(arr)
+    mid = int(len(arr)/2)
+    # print("mid is ", mid)
+    if mid == 1 and len(arr) == 2:
+        return arr[mid]
+    else:
+        if arr[mid] <= arr[mid+1]:
+            # print("case1")
+            return find_peak(arr[mid:])
+        elif arr[mid] <= arr[mid - 1]:
+            # print("case 2")
+            return find_peak(arr[:mid])
+        else:
+            return arr[mid]
+
+
+
+print("-----------------------------------")
+print("The peak for the sequence is : ", find_peak(arr) )
+print("-----------------------------------")
+
 
