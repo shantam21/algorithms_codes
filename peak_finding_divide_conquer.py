987f7a529d17723e51d183a989c167b345fb4351
shansaxena <shansaxena@deloitte.com>
2020-04-26 21:03:13 +0530

    first commit test

---
+++ peak_finding_divide_conquer.py
@@ -0,0 +1,12 @@
+# for monotonically inceeasing then decreasing sequence of arrays
+
+arr = [2,4,6,7,8,5,4,1]
+
+def find_peak(arr):
+"""[summary]
+
+Arguments:
+    arr {[type]} -- [description]
+"""
+    
+
